9c381321b7f297a30a5d9d4e61d0fc6cca6602a0
Loris Dié <loris.die@converteo.com>
2023-11-07 16:31:05 +0100

    feat (main) : number to roman 1 to 5

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+const numberToRoman = (number) => {
+  const romanNumbers = {
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let roman = "";
+
+  for (let i in romanNumbers) {
+    while (number >= romanNumbers[i]) {
+      roman += i;
+      number -= romanNumbers[i];
+    }
+  }
+  return roman;
+};
+
+console.log(numberToRoman(4));
