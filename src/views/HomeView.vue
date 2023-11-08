5e3ad9afba3f8ab40b22da802a5b7c01e73aee6d
Amine Zegmou <63230216+ZeAmine@users.noreply.github.com>
2023-11-08 13:54:09 +0100

    feat(main): Added function romanToNumber

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <template>
   <main class="main">
-    <h1>Hello</h1>
+    <div class="home">
+      <div class="block">
+        <!-- <label for="arabicNbr">Choissez un chiffre (1 à 3999)</label>
+        <input type="number" id="arabicNbr" v-model="arabicNbr" required />
+        <p>{{ romanNbr }}</p> -->
+
+        <label for="romanNbr">Choissez un chiffre (1 à 3999)</label>
+        <input type="text" id="romanNbr" v-model="romanNbr" required />
+        <p>{{ arabicNbr }}</p>
+      </div>
+    </div>
   </main>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from "vue";
+import { onMounted, ref, watchEffect } from "vue";
+
+const arabicNbr = ref<number>(1);
+const romanNbr = ref<number>("V");
 
 function numberToRoman(number: number): string {
   if (number % 1 !== 0 || number <= 0 || number >= 4000) {
@@ -18,9 +31,9 @@ function numberToRoman(number: number): string {
     D: 500,
     CD: 400,
     C: 100,
-    XCIX: 99,
+    XC: 90,
     L: 50,
-    XLIX: 49,
+    XL: 40,
     X: 10,
     IX: 9,
     V: 5,
@@ -36,11 +49,56 @@ function numberToRoman(number: number): string {
       number -= romanNumbers[i];
     }
   }
-  return roman;
+
+  return (romanNbr.value = roman);
 }
 
+function romanToNumber(roman: string): number {
+  const romanNumbers = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let number = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+    const current = romanNumbers[roman[i]];
+    const next = romanNumbers[roman[i + 1]];
+
+    if (next && current < next) {
+      number -= current;
+    } else {
+      number += current;
+    }
+  }
+
+  return (arabicNbr.value = number);
+}
+
+watchEffect(() => {
+  if (arabicNbr.value) {
+    numberToRoman(arabicNbr.value);
+  }
+
+  if (romanNbr.value) {
+    romanToNumber(romanNbr.value);
+  }
+});
+
 onMounted(() => {
-  numberToRoman(5);
+  numberToRoman(1);
+  romanToNumber("V");
 });
 </script>
 
