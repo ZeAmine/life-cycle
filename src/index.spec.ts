0527f3a161809bea7926dc8a115ad82b15127719
Amine Zegmou <63230216+ZeAmine@users.noreply.github.com>
2023-11-08 09:38:14 +0100

    feat(main): Added test for the function

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,32 +1,26 @@
 // @ts-ignore see https://github.com/jest-community/jest-extended#setup
 import * as matchers from "jest-extended";
-import fc from "fast-check";
 
-expect.extend(matchers);
-
-test("A simple test (Jest)", () => {
-  expect(1 + 1).toEqual(2);
+test("numberToRoman should convert integer to Roman numeral", () => {
+  expect(numberToRoman(1)).toEqual("I");
+  expect(numberToRoman(4)).toEqual("IV");
+  expect(numberToRoman(9)).toEqual("IX");
+  expect(numberToRoman(42)).toEqual("XLII");
+  expect(numberToRoman(100)).toEqual("C");
+  expect(numberToRoman(500)).toEqual("D");
+  expect(numberToRoman(1000)).toEqual("M");
 });
 
-test("Additional matchers (jest-extended)", () => {
-  expect([1, 0]).toIncludeSameMembers([0, 1]);
+test("numberToRoman should return error message for non-integer or zero", () => {
+  expect(numberToRoman(0)).toEqual("le format du chiffre n'est pas le bon");
+  expect(numberToRoman(3.14)).toEqual("le format du chiffre n'est pas le bon");
 });
 
-test("Property-based testing (fast-check)", () => {
-  type Boundaries = {
-    min: number;
-    max: number;
-  };
+test("numberToRoman should handle large numbers", () => {
+  expect(numberToRoman(1987)).toEqual("MCMLXXXVII");
+  expect(numberToRoman(3999)).toEqual("MMMCMXCIX");
+});
 
-  const minmax =
-    ({ min, max }: Boundaries) =>
-    (n: number): number =>
-      Math.min(max, Math.max(min, n));
-
-  fc.assert(
-    fc.property(fc.integer(), (n): boolean => {
-      const result = minmax({ min: 1, max: 10 })(n);
-      return 1 <= result && result <= 10;
-    }),
-  );
+test("numberToRoman should handle negative numbers", () => {
+  expect(numberToRoman(-1)).toEqual("le format du chiffre n'est pas le bon");
 });
